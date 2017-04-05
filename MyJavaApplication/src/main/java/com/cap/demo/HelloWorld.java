5426ec19a0c7633df7a7d4d9f34ca1fc224c4a5a
ns6 <naveen.b.s.capgemini.com>
2017-04-05 19:08:54 +0530

    Commited the helloworld

--- MyJavaApplication/src/main/java/com/cap/demo/HelloWorld.java
+++ MyJavaApplication/src/main/java/com/cap/demo/HelloWorld.java
@@ -2,10 +2,17 @@ package com.cap.demo;
 
 public class HelloWorld {
 
-	
+	public void add()
+	{
+		int a =2 ,b=3;
+		int c = a+b;
+		System.out.println(c);
+	}
 	public static void main (String [] args)
 	
 	{
 		System.out.println("helloworld");
+		HelloWorld hw = new HelloWorld();
+		hw.add();
 	}
 }
